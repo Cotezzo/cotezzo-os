364a560b65695fe0bfa0f06ba1434fd18c68b371
Cotezzo <Cotezzo>
2023-12-24 13:17:16 +0100

    ToString unsigned integer implementation

--- bootloader/stage-2/src/lib_to_string.rs
+++ bootloader/stage-2/src/lib_to_string.rs
@@ -1,73 +1,4 @@
-/* Code and comments from: https://os.phil-opp.com/freestanding-rust-binary/  */
-
-/*  This is a bare metal implementation. This means that the final binary won't
-    be linked to the standard library that interacts with the operating system,
-    since there is not operating system (hence, bare metal).
-    no_std tells the compiler not to link the standard library. */
-#![no_std]
-
-/*  Freestanding executable, no access to Rust runtime and crt0.
-    We need to define our own entry point overwriting the crt0 one directly.
-    no_main tells the compiler not to use the normal entry point chain. */
-#![no_main]
-
-/*  Add the message() method to the PanicInfo struct in order to retrieve the
-    reason and print it to the screen when the panic_handler is triggered. */
-#![feature(panic_info_message)]
-
-/* ==== ENTRY POINT ========================================================= */
-/*  All the code written here is underneath the .text._start section.
-    The _start section is then placed above all else by the linker script. */
-    core::arch::global_asm!(".section .text._start");
-
-    mod lib_to_string;
-    use crate::lib_to_string::print_string;
-    use crate::lib_to_string::ToString;
-    
-/*  A main doesn’t make sense without an underlying runtime that calls it.
-    We are overwriting the os entry point with our own _start function.
-    The no_mangle attribute ensures that the compiler outputs the
-    function with name _start and not some cryptic unique name symbol.
-    Required since we need to tell the entry point name to the linker.
-    "_start" is the default entry point name for most systems.
-    We also have to mark the function as extern "C" to tell the compiler that
-    it should use the C calling convention for this function.
-    (https://en.wikipedia.org/wiki/X86_calling_conventions) */
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    // DS, SS, SS registries are set to 0x1000, SP is set to 0 by the stage-1.
-    // Since the Stack Pointer grows backwards, it will wrap around the segment.
-    // The stack will still override our code if we reach 64kB.
-    //! In this environment there are no "stack overflow" guards
-    
-    // Only works in --release...
-    print!("u8: ", 8u8, " u16: ", 16u16, " u32: ", 32u32, " u64: ", 64u64);
-
-    // Do nothing until the end of time
-    loop {}
-}
-
-/* ==== PANIC HANDLER ======================================================= */
-use core::panic::PanicInfo;
-
-/*  panic_handler defines the method that is invoked when a panic occurs.
-    In a no_std environment we need to define it ourselves. */
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-
-    // Print panic reason
-    //print_s(_info.message().unwrap().as_str().expect("Panic!"));
-    print!("Panic!");
-
-    // Do nothing until the end of time
-    loop {}
-}
-
-
-
-
-
-/* ==== TEST ================================================================ *
+/* ==== PUBLIC PRINT MACRO ================================================== */
 /*  Export print macro to print any ToString implementing type. */
 #[macro_export]
 macro_rules! print {
@@ -82,11 +13,25 @@ macro_rules! print {
     };
 }
 
+/* ==== PRIVATE ASM PRINT - needs to be imported for the macro to work ====== */
+/*  Reference a method not located in this Rust module, but search for it
+    link-time. In this case, this is a global method declared in print.asm.
+    Rust calls the method using the C calling convention (extern "C"). */
+extern "C" { fn _print_char(char: u8, page: u8) -> (); }
+
+/* Print each character in the array (ASCII string) using the ASM method. */
+pub fn print_string(s: &str) {
+    for c in s.as_bytes().iter() {
+        unsafe { _print_char(*c, 0); }
+    }
+}
+
+/* ==== PRIVATE TRAIT IMPLEMENTATION ======================================== */
 /*  Define ToString trait so that we can implement a custom to_string function
     for each type we need to print with the print! macro.
     The trait is implement by defining a to_string method that returns a
     reference to a static string. */
-trait ToString { fn to_string(&self) -> &'static str; }
+pub trait ToString { fn to_string(&self) -> &'static str; }
 
 /*  To implement the conversion to unsigned integer to string, we can use the
     same logic for each type, so a macro is used to generate the same impl
@@ -151,4 +96,3 @@ to_string_impl_uint!(u32, 10);
 /*  Implement the ToString trait for u64 using the to_string_impl_uint macro.
     Max size: 20 chars (0-18_446_744_073_709_551_615). */
 to_string_impl_uint!(u64, 20);
-*/
